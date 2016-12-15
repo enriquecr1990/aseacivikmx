7e5f60f96e2bce997bc24a3cad9f3dd1a3b0268d
Enrique Corona <enrique_cr1990@hotmail.com>
2016-12-14 23:59:44 -0600

    primer commit de prueba

--- application/config/database.php
+++ application/config/database.php
@@ -70,6 +70,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
+
+/**
+ * configuracion de la BD conforme al servidor
+ */
 $active_group = 'default';
 $query_builder = TRUE;
 
